1852b27ed9c4be121c32b88c51b9f76ad2afaa1f
Kan Seung Gi <kansg922@gmail.com>
2022-03-31 15:17:57 +0900

    login-Event Part Two

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 
 function onLoginSubmit(event){
     event.preventDefault();
     console.log(event);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event);
+}
 
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
 

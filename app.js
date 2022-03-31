276d80e3dc60c07dea091d1b2a717f297b308aa8
Kan Seung Gi <kansg922@gmail.com>
2022-03-31 15:47:35 +0900

    login-Geeting Username

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
-
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(event);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText ="Hello " + username;
+    greeting.innerText =`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
-}
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
 

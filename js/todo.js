672a635f4c7a6d4846ae536a88c2190370cceb4a
Kan Seung Gi <kansg922@gmail.com>
2022-04-01 21:19:05 +0900

    7.todo__deletingToDos

--- js/todo.js
+++ js/todo.js
@@ -1,38 +1,45 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input")
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
 const TODOS_KEY = "todos"
 
-let toDos = [];
+let toDos = []; 
 
 function saveToDos(){
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //JSON.stringify() => 값을 모두 String으로 바꿔주는 함수
-}
+};
 
 function deleteToDo(event){
     const li = event.target.parentElement; // click하였을때 나오는 버튼의 부모를 지정하여 준다.
     li.remove();
-}
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
+};
 
 function paintToDo(newToDo){
-    const li = document.createElement("li")
-    const span = document.createElement("span")
-    span.innerText = newToDo;
+    const li = document.createElement("li");
+    li.id = newToDo.id;
+    const span = document.createElement("span");
+    span.innerText = newToDo.text;
     const button = document.createElement("button");
     button.innerText = "❌"
     button.addEventListener("click", deleteToDo)  // 클릭하면 deleteToDo가 실행됨.
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
-}
+};
 
 function handelToDoSubmit(event){
     event.preventDefault();    
     const newToDo = toDoInput.value;
-    toDoInput.value ="a";   
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    toDoInput.value ="";  
+    const newToDoObj = {
+        text : newToDo,
+        id : Date.now(),
+    };
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 };
 
@@ -43,13 +50,12 @@ toDoForm.addEventListener("submit", handelToDoSubmit);
 // }
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
-
-if(saveToDos !== null ){
+if(savedToDos !== null ){
     const parseToDos = JSON.parse(savedToDos);
-    console.log(saveToDos);
-    if(parseToDos !== null ){
     toDos = parseToDos;
-    parseToDos.forEach(paintToDo); 
-    }    
+    parseToDos.forEach(paintToDo);
 };
+
+
+function sexyFilter(){ };
 

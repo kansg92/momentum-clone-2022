3f647bbfbac6c84046ad7daebadd69c713b7670c
Kan Seung Gi <kansg922@gmail.com>
2022-04-01 15:20:55 +0900

    7.todo__addingToDO

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newToDo){
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li);
+}
+
+
 function handelToDoSubmit(event){
-    event.preventDefault();    // html기본 event 리셋
-    const newToDo = toDoInput.value; //toDoInput.value 를 newToDo에담음
-    console.log(toDoInput.value); //toDoInput.value 값 배출 => 현재(newToDo === toDoInput.value)
-    toDoInput.value ="";   //toDoInput.value 값 공백으로 만듦 . => newTodo !== toDoInput.value 기존 value 값달라짐.
-    console.log(newToDo,toDoInput.value);   // 출력값 : 기존 입력갑 + 공백 나옴.
+    event.preventDefault();    
+    const newToDo = toDoInput.value;
+    toDoInput.value ="";   
+    paintToDo(newToDo);
 };
 
 toDoForm.addEventListener("submit", handelToDoSubmit);

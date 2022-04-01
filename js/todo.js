7c9ebcdbaea7f62e09d1b0be85f953b6926011a7
Kan Seung Gi <kansg922@gmail.com>
2022-04-01 15:39:01 +0900

    7.todo__deletingToDo

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement; // click하였을때 나오는 버튼의 부모를 지정하여 준다.
+    li.remove();
+}
+
 function paintToDo(newToDo){
     const li = document.createElement("li")
     const span = document.createElement("span")
-    li.appendChild(span);
     span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "❌"
+    button.addEventListener("click", deleteToDo)  // 클릭하면 deleteToDo가 실행됨.
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
